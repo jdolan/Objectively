b4efb50e99fe6308d0362a753d3c8d0e6f7acf40
Jay Dolan <jay@jaydolan.com>
2014-08-30 12:05:46 -0400

    Here we go.

---
+++ src/objectively.h
@@ -0,0 +1,102 @@
+/*
+ * Objectively: Ultra-lightweight object oriented framework for c99.
+ * Copyright (C) 2014 Jay Dolan <jay@jaydolan.com>
+ *
+ * This software is provided 'as-is', without any express or implied
+ * warranty. In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ * 1. The origin of this software must not be misrepresented; you must not
+ * claim that you wrote the original software. If you use this software
+ * in a product, an acknowledgment in the product documentation would be
+ * appreciated but is not required.
+ *
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ * misrepresented as being the original software.
+ *
+ * 3. This notice may not be removed or altered from any source distribution.
+ */
+
+#ifndef _objectively_h_
+#define _objectively_h_
+
+#include <stdbool.h>
+#include <stdlib.h>
+
+/*
+ * @brief Define the interface (methods and members) of your type.
+ */
+#define Interface(Type, SuperType) \
+	typedef struct _##Type Type; \
+	extern Type __##Type; \
+	struct _##Type { \
+		SuperType super; \
+		\
+		void (*dealloc)(Type *self);
+
+/*
+ * @brief Implement the constructor for your type.
+ */
+#define Implementation(Type, ...) \
+	Type __##Type; \
+	Type *Type##_init(Type *self, ## __VA_ARGS__) {
+
+/*
+ * @brief Terminate an interface or implementation block.
+ */
+#define End };
+
+/*
+ * @brief Declare the constructor prototype for your type.
+ */
+#define Constructor(Type, ...) \
+	extern Type *Type##_init(Type *self, ## __VA_ARGS__)
+
+/*
+ * @brief Initialize the reference instance of your type.
+ */
+#define Initialize(Type, ...) \
+	static _Bool initialized; \
+		if (!initialized) { \
+			Type##_init(&__##Type, ## __VA_ARGS__); \
+			initialized = true; \
+		}
+
+/*
+ * @brief Override a supertype method in your type.
+ */
+#define Override(method, implementation) \
+	self->super.method = implementation;
+
+/*
+ * @brief Instantiate your type.
+ */
+#define New(Type, ...) \
+	Type##_init(calloc(1, sizeof(Type)), ## __VA_ARGS__)
+
+/*
+ * @brief Destroy an instance of your type.
+ */
+#define Destroy(obj) \
+	if (obj) { \
+		obj->dealloc(obj); \
+		obj = NULL; \
+	}
+
+/*
+ * @brief The Object base type.
+ */
+Interface(Object, void *)
+
+End
+
+/*
+ * @brief The Object constructor prototype.
+ */
+Constructor(Object);
+
+#endif
---
+++ src/tests/animals.c
@@ -0,0 +1,119 @@
+/*
+ * Objectively: Ultra-lightweight object oriented framework for c99.
+ * Copyright (C) 2014 Jay Dolan <jay@jaydolan.com>
+ *
+ * This software is provided 'as-is', without any express or implied
+ * warranty. In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ * 1. The origin of this software must not be misrepresented; you must not
+ * claim that you wrote the original software. If you use this software
+ * in a product, an acknowledgment in the product documentation would be
+ * appreciated but is not required.
+ *
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ * misrepresented as being the original software.
+ *
+ * 3. This notice may not be removed or altered from any source distribution.
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <check.h>
+
+#include "../src/objectively.h"
+
+Interface(Animal, Object)
+
+	char *genus;
+	char *species;
+
+	char *(*scientificName)(Animal *self);
+
+End
+
+Constructor(Animal, const char *genus, const char *species);
+
+/*
+ * @brief Deallocate an animal.
+ */
+void Animal_dealloc(Animal *self) {
+
+	free(self->genus);
+	free(self->species);
+
+	self->super.dealloc((Object *) self);
+}
+
+/*
+ * @return The Animal's scientific name.
+ */
+char *Animal_scientificName(Animal *self) {
+
+	ck_assert(self->genus);
+	ck_assert(self->species);
+
+	char *name = malloc(strlen(self->genus) + strlen(self->species) + 1);
+	sprintf(name, "%s %s", self->genus, self->species);
+
+	return name;
+}
+
+Implementation(Animal, const char *genus, const char *species)
+
+	Initialize(Animal, "unknown", "unknown");
+
+	if (Object_init((Object *) self)) {
+
+		self->genus = strdup(genus);
+		self->species = strdup(species);
+
+		self->scientificName = Animal_scientificName;
+	}
+
+	return self;
+
+End
+
+START_TEST(zoo)
+	{
+		Animal *lion = New(Animal, "Panthera", "leo");
+		Animal *tiger = New(Animal, "Panthera", "tigris");
+		Animal *bear = New(Animal, "Ursus", "arctos");
+
+		ck_assert_str_eq("Panthera", lion->genus);
+		ck_assert_str_eq("tigris", tiger->species);
+
+		char *name = bear->scientificName(bear);
+		ck_assert_str_eq("Ursus arctos", name);
+		free(name);
+
+		// No animals were actually harmed in the writing of this test
+
+		Destroy(lion);
+		Destroy(tiger);
+		Destroy(bear);
+
+	}END_TEST
+
+int main(int argc, char **argv) {
+
+	TCase *tcase = tcase_create("animals");
+	tcase_add_test(tcase, zoo);
+
+	Suite *suite = suite_create("animals");
+	suite_add_tcase(suite, tcase);
+
+	SRunner *runner = srunner_create(suite);
+
+	srunner_run_all(runner, CK_NORMAL);
+	int failed = srunner_ntests_failed(runner);
+
+	srunner_free(runner);
+
+	return failed;
+}
