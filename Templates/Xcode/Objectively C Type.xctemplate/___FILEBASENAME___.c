d1b5325a4306a49d29a04eb3ac39e92c264e7ef6
Jay Dolan <jay@jaydolan.com>
2016-01-31 12:34:27 -0500

    Xcode templates for Objectively types. Works boss.

---
+++ Templates/Xcode/Objectively C Type.xctemplate/___FILEBASENAME___.c
@@ -0,0 +1,71 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+#include <assert.h>
+
+#include <___PROJECTNAME___/___FILEBASENAMEASIDENTIFIER___.h>
+
+#define _Class ____FILEBASENAMEASIDENTIFIER___
+
+#pragma mark - Object
+
+/**
+ * @see Object::dealloc(Object *)
+ */
+static void dealloc(Object *self) {
+	
+	//..
+	
+	super(___VARIABLE_superclass:identifier___, self, dealloc);
+}
+
+#pragma mark - ___FILEBASENAMEASIDENTIFIER___
+
+/**
+ * @fn ___FILEBASENAMEASIDENTIFIER___ *___FILEBASENAMEASIDENTIFIER___::init(___FILEBASENAMEASIDENTIFIER___ *self)
+ *
+ * @memberof ___FILEBASENAMEASIDENTIFIER___
+ */
+static ___FILEBASENAMEASIDENTIFIER___ *init(___FILEBASENAMEASIDENTIFIER___ *self) {
+	
+	self = (___FILEBASENAMEASIDENTIFIER___ *) super(___VARIABLE_superclass:identifier___, self, init);
+	if (self) {
+		
+		//..
+	}
+	
+	return self;
+}
+
+//..
+
+#pragma mark - Class lifecycle
+
+/**
+ * @see Class::initialize(Class *)
+ */
+static void initialize(Class *clazz) {
+	
+	((ObjectInterface *) clazz->interface)->dealloc = dealloc;
+	
+	((___FILEBASENAMEASIDENTIFIER___Interface *) clazz->interface)->init = init;
+	
+	//..
+}
+
+Class ____FILEBASENAMEASIDENTIFIER___ = {
+	.name = "___FILEBASENAMEASIDENTIFIER___",
+	.superclass = &____VARIABLE_superclass:identifier___,
+	.instanceSize = sizeof(___FILEBASENAMEASIDENTIFIER___),
+	.interfaceOffset = offsetof(___FILEBASENAMEASIDENTIFIER___, interface),
+	.interfaceSize = sizeof(___FILEBASENAMEASIDENTIFIER___Interface),
+	.initialize = initialize,
+};
+
+#undef _Class
+
---
+++ Templates/Xcode/Objectively C Type.xctemplate/___FILEBASENAME___.h
@@ -0,0 +1,75 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+#ifndef ____PROJECTNAME_______FILEBASENAMEASIDENTIFIER____h_
+#define ____PROJECTNAME_______FILEBASENAMEASIDENTIFIER____h_
+
+#include <___PROJECTNAME___/___VARIABLE_superclass:identifier___.h>
+
+/**
+ * @file
+ *
+ * @brief ..
+ */
+
+typedef struct ___FILEBASENAMEASIDENTIFIER___ ___FILEBASENAMEASIDENTIFIER___;
+typedef struct ___FILEBASENAMEASIDENTIFIER___Interface ___FILEBASENAMEASIDENTIFIER___Interface;
+
+/**
+ * @brief The ___FILEBASENAMEASIDENTIFIER___ type.
+ */
+struct ___FILEBASENAMEASIDENTIFIER___ {
+	
+	/**
+	 * @brief The parent.
+	 *
+	 * @private
+	 */
+	___VARIABLE_superclass:identifier___ ___VARIABLE_superclassMember:identifier___;
+	
+	/**
+	 * @brief The typed interface.
+	 *
+	 * @private
+	 */
+	___FILEBASENAMEASIDENTIFIER___Interface *interface;
+	
+	//..
+};
+
+/**
+ * @brief The ___FILEBASENAMEASIDENTIFIER___ interface.
+ */
+struct ___FILEBASENAMEASIDENTIFIER___Interface {
+	
+	/**
+	 * @brief The parent interface.
+	 */
+	___VARIABLE_superclass:identifier___Interface ___VARIABLE_superclassMember:identifier___Interface;
+	
+	/**
+	 * @fn ___FILEBASENAMEASIDENTIFIER___ *___FILEBASENAMEASIDENTIFIER___::init(___FILEBASENAMEASIDENTIFIER___ *self)
+	 *
+	 * @brief Initializes this ___FILEBASENAMEASIDENTIFIER___.
+	 *
+	 * @return The initialized ___FILEBASENAMEASIDENTIFIER___, or `NULL` on error.
+	 *
+	 * @memberof ___FILEBASENAMEASIDENTIFIER___
+	 */
+	___FILEBASENAMEASIDENTIFIER___ *(*init)(___FILEBASENAMEASIDENTIFIER___ *self);
+	
+	// ..
+};
+
+/**
+ * @brief The ___FILEBASENAMEASIDENTIFIER___ Class.
+ */
+extern Class ____FILEBASENAMEASIDENTIFIER___;
+
+#endif /* ____PROJECTNAME_______FILEBASENAMEASIDENTIFIER____h_ */
+
