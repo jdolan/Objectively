caceb4b1301361cb77fd875932528595db0e0d9d
Jay Dolan <jay@jaydolan.com>
2016-08-28 20:34:14 -0400

    Introduce Value type for Object encapsulation of C types. This allows adding C structs to Collections, etc.

---
+++ Sources/Objectively/Value.c
@@ -0,0 +1,115 @@
+/*
+ * Objectively: Ultra-lightweight object oriented framework for GNU C.
+ * Copyright (C) 2014 Jay Dolan <jay@jaydolan.com>
+ *
+ * This software is provided 'as-is', without any express or implied
+ * warranty. In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ * 1. The origin of this software must not be misrepresented; you must not
+ * claim that you wrote the original software. If you use this software
+ * in a product, an acknowledgment in the product documentation would be
+ * appreciated but is not required.
+ *
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ * misrepresented as being the original software.
+ *
+ * 3. This notice may not be removed or altered from any source distribution.
+ */
+
+#include <assert.h>
+#include <stdlib.h>
+
+#include <Objectively/Hash.h>
+#include <Objectively/Value.h>
+
+#define _Class _Value
+
+#pragma mark - Object
+
+/**
+ * @see Object::dealloc(Object *)
+ */
+static void dealloc(Object *self) {
+	
+	Value *this = (Value *) self;
+
+	if (this->freeOnDealloc) {
+		free(this->value);
+	}
+	
+	super(Object, self, dealloc);
+}
+
+/**
+ * @see Object::hash(const Object *)
+ */
+static int hash(const Object *self) {
+
+	const Value *this = (Value *) self;
+
+	return super(Object, self, hash) + (intptr_t) this->value;
+}
+
+/**
+ * @see Object::isEqual(const Object *, const Object *)
+ */
+static _Bool isEqual(const Object *self, const Object *other) {
+
+	if (other && $(other, isKindOfClass, &_Value)) {
+
+		const Value *this = (Value *) self;
+		const Value *that = (Value *) other;
+
+		return this->value == that->value;
+	}
+
+	return false;
+}
+
+#pragma mark - Value
+
+/**
+ * @fn Value *Value::initWithValue(Value *self, ident value)
+ *
+ * @memberof Value
+ */
+static Value *initWithValue(Value *self, ident value) {
+	
+	self = (Value *) super(Object, self, init);
+	if (self) {
+		self->value = value;
+	}
+	
+	return self;
+}
+
+#pragma mark - Class lifecycle
+
+/**
+ * @see Class::initialize(Class *)
+ */
+static void initialize(Class *clazz) {
+	
+	((ObjectInterface *) clazz->interface)->dealloc = dealloc;
+	((ObjectInterface *) clazz->interface)->hash = hash;
+	((ObjectInterface *) clazz->interface)->isEqual = isEqual;
+	
+	((ValueInterface *) clazz->interface)->initWithValue = initWithValue;
+}
+
+Class _Value = {
+	.name = "Value",
+	.superclass = &_Object,
+	.instanceSize = sizeof(Value),
+	.interfaceOffset = offsetof(Value, interface),
+	.interfaceSize = sizeof(ValueInterface),
+	.initialize = initialize,
+};
+
+#undef _Class
+
---
+++ Sources/Objectively/Value.h
@@ -0,0 +1,99 @@
+/*
+ * Objectively: Ultra-lightweight object oriented framework for GNU C.
+ * Copyright (C) 2014 Jay Dolan <jay@jaydolan.com>
+ *
+ * This software is provided 'as-is', without any express or implied
+ * warranty. In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ * 1. The origin of this software must not be misrepresented; you must not
+ * claim that you wrote the original software. If you use this software
+ * in a product, an acknowledgment in the product documentation would be
+ * appreciated but is not required.
+ *
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ * misrepresented as being the original software.
+ *
+ * 3. This notice may not be removed or altered from any source distribution.
+ */
+
+#pragma once
+
+#include <Objectively/Object.h>
+
+/**
+ * @file
+ *
+ * @brief The Value type.
+ */
+
+typedef struct Value Value;
+typedef struct ValueInterface ValueInterface;
+
+/**
+ * @brief Values provide Object encapsulation for C types.
+ *
+ * @extends Object
+ *
+ * @ingroup
+ */
+struct Value {
+	
+	/**
+	 * @brief The parent.
+	 *
+	 * @private
+	 */
+	Object object;
+	
+	/**
+	 * @brief The typed interface.
+	 *
+	 * @private
+	 */
+	ValueInterface *interface;
+
+	/**
+	 * @brief The backing value.
+	 */
+	ident value;
+
+	/**
+	 * @brief If true, the backing value will be freed on `dealloc`.
+	 */
+	_Bool freeOnDealloc;
+};
+
+/**
+ * @brief The Value interface.
+ */
+struct ValueInterface {
+	
+	/**
+	 * @brief The parent interface.
+	 */
+	ObjectInterface objectInterface;
+	
+	/**
+	 * @fn Value *Value::initWithValue(Value *self, ident value)
+	 *
+	 * @brief Initializes this Value.
+	 *
+	 * @param value The backing value.
+	 *
+	 * @return The initialized Value, or `NULL` on error.
+	 *
+	 * @memberof Value
+	 */
+	Value *(*initWithValue)(Value *self, ident value);
+};
+
+/**
+ * @brief The Value Class.
+ */
+extern Class _Value;
+
