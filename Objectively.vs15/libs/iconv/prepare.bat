def31a4ca8117d39603d78741d66715dbfbc6e38
Paril <paril@alteredsoftworks.com>
2016-09-25 10:01:51 -0400

    Final prep and cleanup.

--- Objectively.vs15/libs/iconv/prepare.bat
+++ Objectively.vs15/libs/iconv/prepare.bat
@@ -1,4 +1,1 @@
-copy vs\config.h.vs15 config.h /Y
-copy include\iconv.h.msvc-static include\iconv.h /Y
-copy vs\lib.config.h.vs15 lib\config.h /Y
-copy libcharset\include\localcharset.h.in libcharset\include\localcharset.h /Y
+move windows\stdbool.h windows\stdbool.h_
