3cbc4833af4c9d314908675d3324015342492445
Paril <paril@alteredsoftworks.com>
2016-09-30 09:18:21 -0400

    Testing something

---
+++ Objectively.vs15/libs/curl/include/curl/PREPARE.ps1
@@ -0,0 +1,6 @@
+Write-Output "Downloading cURL..."
+(New-Object System.Net.WebClient).DownloadFile("https://curl.haxx.se/download/curl-7.50.3.tar.gz", "tmp.tar.gz")
+Write-Output "Extracting cURL..."
+7z x tmp.tar.gz
+7z e tmp.tar -aos "curl-7.50.3\include\curl\*.h"
+Remove-Item "tmp.*"
