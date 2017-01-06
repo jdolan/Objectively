e214d5b816a432e9d525e45baa8f161906cc587e
Jay Dolan <jay@jaydolan.com>
2017-01-06 09:35:03 -0500

    Doxygen.

--- Sources/Objectively/Once.h
+++ Sources/Objectively/Once.h
@@ -38,6 +38,7 @@ typedef long Once;
 
 /**
  * @brief Executes the given `block` at most one time.
+ * @ingroup Concurrency
  */
 #define do_once(once, block) \
 		if (__sync_val_compare_and_swap(once, 0, -1) == 0) { \
