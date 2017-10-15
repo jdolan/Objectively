15a4085108475def1cf2617ae152fb02e6f32b01
Paril <paril@alteredsoftworks.com>
2017-10-15 11:29:53 -0400

    Missed the config file

---
+++ Objectively.vs15/Sources/gnu/config.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#define __USE_GNU
+
+#define CODESET 0
+
+#if defined(_UNICODE)
+ static const char *nl_langinfo(int c)
+ {
+	 return "UTF-16";
+ }
+#elif defined(_MBCS)
+ static const char *nl_langinfo(int c)
+ {
+	 return "ASCII";
+ }
+#else
+ #error No charset?
+#endif
