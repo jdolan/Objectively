c0284df48ede0aa69fe0ce6a27ffc9725b54ee95
jdolan <jay@jaydolan.com>
2018-02-06 20:46:26 -0500

    Use Config.h to define _GNU_SOURCE. This is the 'right' way.

--- Sources/Objectively/MutableArray.c
+++ Sources/Objectively/MutableArray.c
@@ -21,6 +21,8 @@
  * 3. This notice may not be removed or altered from any source distribution.
  */
 
+#include <Objectively/Config.h>
+
 #include <assert.h>
 #include <stdarg.h>
 #include <stdlib.h>
