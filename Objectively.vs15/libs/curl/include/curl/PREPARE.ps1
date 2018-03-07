99403a99c6defc9521bce03b7774735fe14c7de8
Paril <me@paril.ca>
2018-03-07 17:23:10 -0500

    Add dependencies in here to make things simpler and not need preparing. Todo: regex is heavy, I can move this to a static lib and just bundle that.

--- Objectively.vs15/libs/curl/include/curl/PREPARE.ps1
+++
@@ -1,9 +0,0 @@
-If (!(Test-Path tmp7551.tar.gz)) {
-    Write-Output "Downloading cURL..."
-    (New-Object System.Net.WebClient).DownloadFile("https://github.com/curl/curl/releases/download/curl-7_55_1/curl-7.55.1.tar.gz", "tmp7551.tar.gz")
-}
-
-Write-Output "Extracting cURL..."
-7z x tmp7551.tar.gz
-7z e tmp7551.tar -aos "curl-7.55.1\include\curl\*.h"
-Remove-Item "tmp7551.tar"
